e79aa7d23a0157894086ec466ab1fa4b57ccfe51
Zyena Kamran <zyena.intern@mrs-electronic.com>
2023-09-13 04:31:01 +0500

    search result screen

---
+++ app/src/main/java/com/example/i200802_a1/Home.java
@@ -0,0 +1,13 @@
+package com.example.i200802_a1;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Home extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
